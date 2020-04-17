ad164ee7fb3c4d4c3b742ad8fa05deba6b0e6ee9
Gumball12 <kan02134@gmail.com>
2020-04-17 17:04:45 +0900

    Feat: Add 404 page

---
+++ src/components/PNF.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="py-12">
+    <h2 class="py-12 grey--text text--lighten-3 text-center"><span>' ~ ^</span><br>Wrong page</h2>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+span {
+  font-family: fantasy !important;
+  font-size: 18px;
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -10,14 +10,24 @@ const routes = [
     component: () => import('../views/Home.vue'),
   },
   {
-    path: '/:title',
+    path: '/exhibition/:title',
     name: 'Exhibition',
     component: () => import('../views/Exhibition.vue'),
+  },
+  {
+    path: '*',
+    name: 'PNF',
+    component: () => import('../components/PNF.vue'),
   },
 ];
 
 const router = new VueRouter({
   routes,
+});
+
+router.beforeEach((to, from, next) => {
+  window.scrollTo(0, 0);
+  next();
 });
 
 export default router;
